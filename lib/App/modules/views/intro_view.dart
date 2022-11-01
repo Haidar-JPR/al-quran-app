f5cd74a06ca8f260fcb82e5c2b4a4d2cfb36c90a
haidar <haidar.dod@gmail.com>
2022-11-02 06:45:53 +0700

    adding introdution view, home page, assets and lotties folder, get and lottie package, active assets pubspec

--- lib/App/modules/views/intro_view.dart
+++ lib/App/modules/views/intro_view.dart
@@ -1,1 +1,46 @@
+import 'package:al_quran_app/App/modules/views/home.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:lottie/lottie.dart';
+import 'package:get/get.dart';
+
+class IntrodutionView extends StatelessWidget {
+  const IntrodutionView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Al-Quran Apps",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 26,
+              ),
+            ),
+            SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: Text(
+                'Sesibuk itukah kamu sampai belum membaca al-quran ?',
+                style: TextStyle(fontSize: 16),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Lottie.asset('assets/lotties/quran.json', width: 400),
+            SizedBox(height: 30),
+            ElevatedButton(
+              onPressed: () => Get.to(HomePage()),
+              child: Text(
+                'GET STARTED',
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
